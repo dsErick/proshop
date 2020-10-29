2b5b599128fe05be0a4117dc750fb64f77642e8e
Erick Silva <ericksilvablue@gmail.com>
2020-10-29 10:57:56 -0300

    Vue Setup

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+<div class="home">
+    <h1>Welcome to ProShop</h1>
+</div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+    name: 'Home',
+    components: {}
+}
+</script>
