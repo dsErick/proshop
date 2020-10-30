f33f9622cb60e3cd5f509687d92266ae73efd893
Erick Silva <ericksilvablue@gmail.com>
2020-10-29 21:38:06 -0300

    * Bootstrap and jQuery added && Initial bootstrap theme

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,3 +1,16 @@
+const webpack = require('webpack')
+
 module.exports = {
-  lintOnSave: false
+    lintOnSave: false,
+    
+    configureWebpack: {
+        plugins: [
+            new webpack.ProvidePlugin({
+                $: 'jquery',
+                jQuery: 'jquery',
+                'window.jQuery': 'jquery',
+                Popper: ['popper.js', 'default']
+            })
+        ]
+    }
 }
