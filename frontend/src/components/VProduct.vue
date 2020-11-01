39c712695ca0e2e9cf145cfac9b1e6a7d8d06273
Erick Silva <ericksilvablue@gmail.com>
2020-10-31 23:28:03 -0300

    HomeScreen product listing

---
+++ frontend/src/components/VProduct.vue
@@ -0,0 +1,32 @@
+<template>
+<div class="card">
+    <router-link :to="`/products/${product._id}`" class="text-dark">
+        <img :src="product.image" class="card-img-top">
+
+        <div class="card-body">
+            <h5 class="card-title"><strong>{{ product.name }}</strong></h5>
+
+            <div class="card-text my-3">
+                {{ product.rating }} from {{ product.numReviews }} reviews
+            </div>
+
+            <h3 class="card-text my-3">${{ product.price }}</h3>
+        </div>
+    </router-link>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'VProduct',
+    props: {
+        product: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,12 +1,49 @@
 <template>
 <div id="home" class="container-lg container-fluid ">
-    <h1>Welcome to ProShop</h1>
+    <h2>Latest Products</h2>
+
+    <div class="row" v-if="products.length">
+        <div
+            class="col-xl-3 col-lg-4 col-md-6 col-sm-12 mb-3"
+            v-for="product in products"
+            :key="product._id"
+        >
+            <v-product
+                class="h-100"
+                :product="product"
+            />
+        </div>
+    </div>
 </div>
 </template>
 
 <script>
+import { ref, defineAsyncComponent } from 'vue'
+import productsArray from '@/products.js'
+
 export default {
     name: 'Home',
-    components: {}
+    components: {
+        VProduct: defineAsyncComponent(() => import(/* webpackChunkName: "v-product" */ '../components/VProduct'))
+    },
+    setup() {
+        const products = ref(productsArray)
+
+        return {
+            products
+        }
+    }
 }
 </script>
+
+<style scoped lang="scss">
+#home {
+    > .row {
+        margin: 0 -.5rem;
+
+        > div.col-xl-3 {
+            padding: 0 .5rem;
+        }
+    }
+}
+</style>
