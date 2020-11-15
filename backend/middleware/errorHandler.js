f23d5827c3fd73faafc6dd06e32bee927ca6cbd0
Erick Silva <ericksilvablue@gmail.com>
2020-11-15 18:37:18 -0300

    * Properly Mongoose ValidationError and DuplicateKey error response

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -11,15 +11,38 @@ export const errorHandler = (err, req, res, next) => {
     
     let statusCode = res.statusCode === 200 ? 500 : res.statusCode
     let message = err.message
+    let errors = {}
     
-    // Mongoose bad ObjectId
+    // Mongoose bad ObjectId - CastError
     if (err.name === 'CastError') {
         statusCode = 422
         message = `Resource not found with the id of ${err.value}`
     }
     
+    // Mongoose ValidationError
+    if (err.name === 'ValidationError') {
+        statusCode = 422
+        message = `The given data was invalid`
+        
+        Object.keys(err.errors).forEach(key => {
+            if (typeof errors[key] !== Array) errors[key] = []
+
+            errors[key].push(err.errors[key].message)
+        })
+    }
+    
+    // Mongoose unique field duplicate key
+    if (err.code === 11000) {
+        statusCode = 422
+        message = `The given data was invalid`
+
+        const field = Object.keys(err.keyValue)[0]
+        errors[field] = [`The ${field} has already been taken`]
+    }
+    
     res.status(statusCode).json({
         message,
+        errors: Object.keys(errors).length === 0 ? undefined : errors,
         stack: process.env.NODE_ENV === 'production' ? undefined : err.stack
     })
 }
