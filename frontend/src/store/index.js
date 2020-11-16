ee4702fbedd21067848affb40de03438ccdb93ad
Erick Silva <ericksilvablue@gmail.com>
2020-11-16 10:06:15 -0300

    User Login Reducer & Action - Initial loginUser action

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,14 @@ import { createStore } from 'vuex'
 
 import cart from './modules/cart'
 import products from './modules/products'
+import users from './modules/users'
 import utils from './modules/utils'
 
 export default createStore({
     modules: {
         cart,
         products,
+        users,
         utils
     }
 })
---
+++ frontend/src/store/modules/users.js
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import actionHandler from '../actionHandler'
+
+const initialUserState = () => (JSON.parse(localStorage.getItem('user')) ?? {
+    _id: '',
+    name: '',
+    email: '',
+    isAdmin: false,
+})
+
+const state = {
+    user: initialUserState()
+}
+
+const getters = {
+    getUser: state => state.user
+}
+
+const actions = {
+    loginUser: actionHandler(async ({ commit }, { email, password }) => {
+        const { data } = await axios.post('/api/users/login', { email, password })
+
+        localStorage.setItem('user', JSON.stringify(data.data))
+        
+        commit('setUser', data.data)
+        commit('utils/resetError', null, { root: true })
+    }),
+}
+
+const mutations = {
+    setUser: (state, newUser) => Object.keys(newUser).forEach(key => state.user[key] = newUser[key])
+}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+}
