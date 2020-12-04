21e75ee393d99182c1bde32a7ea6fac84cb61830
Erick Silva <ericksilvablue@gmail.com>
2020-12-04 09:40:13 -0300

    * src/views/Order folder renamed to Cart

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -15,25 +15,25 @@ const routes = [
     {
         path: '/cart',
         name: 'Cart',
-        component: () => import(/* webpackChunkName: "cart-view" */ '@/views/Order/Cart.vue')
+        component: () => import(/* webpackChunkName: "cart-view" */ '@/views/Cart/Cart.vue')
     },
     {
         path: '/shipping',
         name: 'Shipping',
         meta: { requiresAuth: true },
-        component: () => import(/* webpackChunkName: "shipping-view" */ '@/views/Order/Shipping.vue')
+        component: () => import(/* webpackChunkName: "shipping-view" */ '@/views/Cart/Shipping.vue')
     },
     {
         path: '/payment',
         name: 'Payment',
         meta: { requiresAuth: true },
-        component: () => import(/* webpackChunkName: "payment-view" */ '@/views/Order/Payment.vue')
+        component: () => import(/* webpackChunkName: "payment-view" */ '@/views/Cart/Payment.vue')
     },
     {
         path: '/placeorder',
         name: 'Place Order',
         meta: { requiresAuth: true },
-        component: () => import(/* webpackChunkName: "place-order-view" */ '@/views/Order/PlaceOrder.vue')
+        component: () => import(/* webpackChunkName: "place-order-view" */ '@/views/Cart/PlaceOrder.vue')
     },
 
     {
